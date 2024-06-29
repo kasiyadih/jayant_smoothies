bc9b48bd971c5dd60855738c6cbe604c564567f4
Jayant <93598618+kasiyadih@users.noreply.github.com>
2024-06-29 18:56:05 +0530

    Update streamlit_app.py
    added ')' to resolve error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ session = cnx.session()
 
 name_on_order = st.text_input('Name on smootie')
 st.write('Your name in smoothie :',name_on_order)
-my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data = my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
 
