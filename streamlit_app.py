f628f8a1f704bf2362ed4d7efe5a99ff625e5eee
Jayant <93598618+kasiyadih@users.noreply.github.com>
2024-06-29 21:00:04 +0530

    Update streamlit_app.py
    # comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ session = cnx.session()
 name_on_order = st.text_input('Name on smootie')
 st.write('Your name in smoothie :',name_on_order)
 my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(my_dataframe)
+#st.dataframe(my_dataframe)
 st.write('check')
 pd_df = my_dataframe.to_pandas()
 
